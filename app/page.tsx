ea6f48acc2edecddcaad46afb7eae4001b232f73
Dalton Lopez <daltonnlopezz@gmail.com>
2025-12-19 14:05:17 -0800

    Fix module resolution: Use relative imports instead of path aliases

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Hero from '@/components/Hero'
-import BrandShowcase from '@/components/BrandShowcase'
-import Features from '@/components/Features'
-import CTA from '@/components/CTA'
+import Hero from '../components/Hero'
+import BrandShowcase from '../components/BrandShowcase'
+import Features from '../components/Features'
+import CTA from '../components/CTA'
 
 export default function Home() {
   return (

028736493438ce98bcd053d437e06ee7c3c89642
Dalton Lopez <daltonnlopezz@gmail.com>
2025-12-19 14:34:46 -0800

    Simplify Next.js app title to match HTML version

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 export const metadata: Metadata = {
-  title: 'G&L Quality Goods - Premium Brand Consolidation',
+  title: 'G&L Quality Goods',
   description: 'Discover premium quality goods from our curated collection of trusted brands',
 }
 

523c4f22b751001463960c4ec9ad5b04d98a590b
Dalton Lopez <daltonnlopezz@gmail.com>
2025-12-19 14:01:59 -0800

    Revert to standard Next.js config for Render

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: false,
-  },
-  output: 'standalone',
 }
 
 module.exports = nextConfig
